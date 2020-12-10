7be476c11cd31e5fb7d0d24c9a5e82860c37a818
Guillem Delas <guillemdelas@hotmail.com>
2020-12-10 12:47:03 +0100

    built the transfer money logic

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const account1 = {
   owner: 'Guillem Delás',
   username: 'guillemdlopez',
   pin: 1111,
+  avatar: 'images/profile.jpg',
   movements: [200, 400, 600, 1000, 10, -230],
 }
 
@@ -13,6 +14,7 @@ const account2 = {
   owner: 'Antonio Morales',
   username: 'amoralro1996',
   pin: 2222,
+  avatar: 'images/antonio-avatar.jpg',
   movements: [50, 600, 1000, 34, 789, -500],
 }
 
@@ -50,8 +52,16 @@ const alertSuccess = document.querySelector('.alert-success');
 const btnCloseAlert = document.querySelectorAll('.close-icon');
 const btnTransfer = document.querySelector('.transfer-money');
 const overlay = document.querySelector('.overlay');
+const modalTransfer = document.querySelector('.transfer-modal');
+const transferForm = document.querySelector('.transfer-modal-form');
+const inputUsernameTransferModal = document.querySelector('.input-username-transfer-modal');
+const inputAmountTransferModal = document.querySelector('.input-amount-transfer-modal');
+console.log(alert);
 
-console.log(welcomeMsg, currentDate);
+
+console.log(inputUsernameTransferModal, inputAmountTransferModal);
+
+// FUNCTIONS
 
 // FANCY BANNER //
 const initBanner = function (el) {
@@ -75,6 +85,72 @@ const bannerObserver = new IntersectionObserver(displayForm, {
 })
 
 bannerObserver.observe(banner)
+
+const findUser = (user) => accounts.find(acc => user === acc.username)
+
+const timer = (sec, htmlElement) => {
+  setTimeout(() => {
+    htmlElement.classList.add('hidden-effect');
+    htmlElement.style.transition = 'all 1s';
+
+    setTimeout(() => {
+      htmlElement.remove();
+    }, 1000)
+  }, sec * 1000)
+}
+
+let customizedAlert;
+const displayWarningAlert = (msg) => {
+  const html = `
+    <div class="alert customized-warning-alert">
+      <i class="fas fa-exclamation-triangle warning"></i>
+      <p>${msg}</p>
+      <i class="close-icon">&times</i>
+    </div>`;
+
+  overlay.insertAdjacentHTML('beforeend', html);
+  customizedAlert = document.querySelector('.customized-warning-alert');
+  timer(2, customizedAlert);
+}
+
+const displaySuccessAlert = (msg) => {
+  const html = `
+    <div class="alert-success customized-success-alert">
+      <i class="fas fa-check-circle success"></i>
+      <p>Correct credentials!</p>
+      <i class="close-icon">&times</i>
+    </div>`;
+    overlay.insertAdjacentHTML('beforeend', html);
+    customizedAlert = document.querySelector('.customized-success-alert');
+    timer(2, customizedAlert);
+}
+let confirmationBtn;
+let cancelBtn;
+const displayConfirmationMsg = (imgPath, amount, userOwner) => {
+  const html = `
+    <div class="modal transfer-modal-confirmation">
+      <div class="info-user">
+        <h2>Confirmation</h2>
+        <img src=${imgPath} class="avatar" alt="">
+        <p><strong>Send</strong>: ${amount} €</p>
+        <p><strong>To</strong>: ${userOwner}</p>
+      </div>
+      <div class="btns-confirmation-cancel">
+        <div class="cancel">
+          <p>Cancel</p>
+        </div>
+        <div class="confirm">
+          <p>Confirm</p>
+        </div>
+      </div>
+    </div>`
+
+    overlay.insertAdjacentHTML('beforeend', html);
+}
+
+// const displayConfirmation(user, amount) => {
+//   const html
+// }
 
 
 // LOG IN //
@@ -119,7 +195,7 @@ loginForm.addEventListener('submit', (e) => {
 
 btnCloseAlert.forEach(btn => {
   btn.addEventListener('click', (e) => {
-    console.log(e.target)
+
     if (e.target.closest('.alert')) {
       alert.classList.add('hidden-effect')
     } else if (e.target.closest('.alert-success')) {
@@ -128,6 +204,39 @@ btnCloseAlert.forEach(btn => {
   })
 })
 
+
 btnTransfer.addEventListener('click', (e) => {
-  overlay.classList.remove('hidden');
+  const btn = e.target.closest('div');
+
+  if (overlay.classList.contains('hidden') && modalTransfer.classList.contains('hidden')) {
+    overlay.classList.remove('hidden');
+    modalTransfer.classList.remove('hidden');
+    inputUsernameTransferModal.focus();
+  } else {
+    overlay.classList.add('hidden');
+    modalTransfer.classList.add('hidden');
+  }
 })
+
+transferForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // console.log(currentAccount);
+  const user = inputUsernameTransferModal.value
+  const amount = inputAmountTransferModal.value
+  const userTo = findUser(user)
+
+  if (userTo) {
+    console.log(`${user} exists!`)
+    modalTransfer.classList.add('move-up');
+    modalTransfer.classList.add('disabled-card');
+    inputAmountTransferModal.blur();
+    displayConfirmationMsg(userTo.avatar, amount, user);
+  } else {
+    console.log(`${user} does not exist!`)
+    displayWarningAlert('Wrong credentials! This account does not exist')
+  }
+})
+
+
+
+
